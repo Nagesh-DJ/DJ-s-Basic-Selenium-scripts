b718f02d38aeaa2b7136167b9a5634f9f743b8bc
Nagesh-DJ <nageshdj.spider@gmail.com>
2025-01-07 17:09:51 +0530

    added cookie Class

--- SeleniumPractice/src/test/java/com/practice/selenium/b_OpenAmazon.java
+++ SeleniumPractice/src/test/java/com/practice/selenium/b_OpenAmazon.java
@@ -1,18 +1,32 @@
 package com.practice.selenium;
 
 import java.time.Duration;
+import java.util.Set;
 
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class b_OpenAmazon {
 
 	public static void main(String[] args) throws Exception
 	{
-		WebDriver driver=new ChromeDriver();
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--incognito");
+		options.addArguments("--disable-automation");
+		options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+		WebDriver driver=new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://www.amazon.com/");
+		driver.manage().deleteAllCookies();
+		Set<Cookie> cookies = driver.manage().getCookies();
+		System.out.println(cookies.size());
+		for(Cookie k:cookies)
+		{
+			System.out.println(k);
+		}
 		Thread.sleep(3000);
 		driver.quit();
 	}
