d83a1f1b654141fd5041c6934aadcf238748ee17
Nagesh-DJ <nageshdj.spider@gmail.com>
2025-01-07 16:57:55 +0530

    added extra date after 30 days

--- SeleniumPractice/src/test/java/com/practice/selenium/aa_TimeDate.java
+++ SeleniumPractice/src/test/java/com/practice/selenium/aa_TimeDate.java
@@ -1,6 +1,7 @@
 package com.practice.selenium;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class aa_TimeDate {
@@ -12,5 +13,11 @@ public static void main(String[] args) {
 	//to get current date 
 	LocalDateTime now = LocalDateTime.now();
 	System.out.println(now);
+	LocalDateTime plusDays = now.plusDays(30);
+	
+	//to get after 30 days
+	DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd-MM-YYYY");
+	String date1 = plusDays.format(formatter);
+	System.out.println(date1);
 }
 }
